6e5689bf2f25ced1222150544b5cd9d7b0517a88
Qadeer Ahmad <qadeer@najoomi.com>
2016-09-06 01:21:02 +0500

    Basic files

---
+++ src/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace YouTube;
+
+use YouTube\Config;
+/**
+ * YouTube Client is used as autoloader and for basic configuration files.
+ *
+ * @author Qadeer Ahmad <qadeer@najoomi.com> 
+ */
+class Client {
+    //put your code here
+}
---
+++ src/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace YouTube;
+
+/**
+ * This class will use to store YouTube Client configuration.
+ *
+ * @author Qadeer Ahmad <qadeer@najoomi.com>
+ * 
+ */
+class Config {
+    //put your code here
+}
